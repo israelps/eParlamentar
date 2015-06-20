f7ec4bebe1323e3b498599aed922e81538be186e
Israel P. Siqueira <israelps@gmail.com>
2015-06-20 03:12:17 -0300

    INIT

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+from models import db
+from models import User
+
 app = Flask(__name__)
+app.config.from_pyfile('config.py', silent=True)
+app.jinja_env.line_statement_prefix = '#'
+db.init_app(app)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/<name>')
+def hello(name=None):
+    return render_template('index.html', name=name)
+
+
+@app.route('/usuario')
+def usuario():
+	u = User("Israel", "israelps@gmail.com")
+	db.session.add(u)
+	db.session.commit()
+	return render_template('usuario.html', usuario=u)
+
 
 if __name__ == '__main__':
+
     app.run()
