7e3c189e248b14774e45cd15dff7066a58caf12a
Israel P. Siqueira <israelps@gmail.com>
2015-06-21 02:03:44 -0300

    commit

--- flaskapp.py
+++ flaskapp.py
@@ -1,22 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from models import Despesa
+from models import Deputado
 from models import db
-from models import User
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py', silent=True)
-app.jinja_env.line_statement_prefix = '##'
+# app.jinja_env.line_statement_prefix = '##'
 db.init_app(app)
 
 
 @app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('index.html', name=name)
+def hello():
+    return render_template('index.html')
 
 
-@app.route('/usuario')
-def usuario():
-    return render_template('usuario.html', usuarios=User.query.all())
+@app.route('/deputados')
+def deputados():
+    deputados = Deputado.query.all()
+    return render_template('deputados.html', deputados=deputados)
+
+
+@app.route('/deputados/<int:ideCadastro>')
+def deputado(ideCadastro):
+    deputado = Deputado.query.filter_by(ideCadastro=ideCadastro).first()
+    return render_template('deputado.html', deputado=deputado)
+
+
+@app.route('/atualizaDeputados')
+def atualiza_deputados():
+    Deputado.atualiza_database()
+    return redirect('/deputados')
+
+@app.route('/atualizaDespesas')
+def atualiza_despesas():
+    Despesa.atualiza_database()
+    return redirect('/deputados')
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,35 +1,26 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask import g
+import urllib2
+from lxml import etree
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
 class Deputado(db.Model):
     ideCadastro = db.Column(db.Integer, primary_key=True)
-    nomeParlamentar = db.Column(db.String(80))
-    nome = db.Column(db.String(100))
-    condicao = db.Column(db.String(20))
-    uf = db.Column(db.String(2))
-    partido = db.Column(db.String(5))
-    gabinete = db.Column(db.Integer)
-    anexo = db.Column(db.Integer)
-    fone = db.Column(db.String(10))
-    email = db.Column(db.String(30))
-    url_foto = db.Column(db.String(100))
+    nomeParlamentar = db.Column(db.String(200))
+    nome = db.Column(db.String(200))
+    condicao = db.Column(db.String(60))
+    uf = db.Column(db.String(3))
+    partido = db.Column(db.String(10))
+    gabinete = db.Column(db.String(5))
+    anexo = db.Column(db.String(5))
+    fone = db.Column(db.String(15))
+    email = db.Column(db.String(200))
+    url_foto = db.Column(db.String(300))
 
-    def __init__(self, ideCadastro, nomeParlamentar, nome, condicao, uf, partido, gabinete, anexo, fone, email, url_foto):
+    def __init__(self, ideCadastro, nomeParlamentar, nome, condicao, uf, partido, gabinete, anexo, fone, email,
+                 url_foto):
         self.ideCadastro = ideCadastro
         self.nomeParlamentar = nomeParlamentar
         self.nome = nome
@@ -42,5 +33,78 @@ class Deputado(db.Model):
         self.email = email
         self.url_foto = url_foto
 
-        def __repr__(self):
-            return '<Deputado %r>' % self.nomeParlamentar
+    def __repr__(self):
+        return '<Deputado %r>' % (self.nomeParlamentar)
+
+    @classmethod
+    def atualiza_database(self):
+        data = urllib2.urlopen("http://www.camara.gov.br/SitCamaraWS/Deputados.asmx/ObterDeputados").read()
+        deputados = etree.XML(data)
+
+        for deputado in deputados:
+            if not Deputado.query.filter_by(ideCadastro=int(deputado.find('ideCadastro').text)).first():
+                ideCadastro = int(deputado.find('ideCadastro').text)
+                nome = deputado.find('nome').text
+                nomeParlamentar = deputado.find('nomeParlamentar').text
+                condicao = deputado.find('condicao').text
+                uf = deputado.find('uf').text
+                partido = deputado.find('partido').text
+                gabinete = deputado.find('gabinete').text
+                anexo = deputado.find('anexo').text
+                fone = deputado.find('fone').text
+                email = deputado.find('email').text
+                url_foto = deputado.find('urlFoto').text
+                dep = Deputado(ideCadastro, nomeParlamentar, nome, condicao, uf, partido, gabinete, anexo, fone, email,
+                               url_foto)
+                db.session.add(dep)
+
+        db.session.commit()
+
+
+class Despesa(db.Model):
+    numRessarcimento = db.Column(db.Integer, primary_key=True)
+    sgUF = db.Column(db.String(2))
+    sgPartido = db.Column(db.String(2))
+    txtDescricao = db.Column(db.String(200))
+    txtFornecedor = db.Column(db.String(200))
+    vlrLiquido = db.Column(db.Float)
+    numMes = db.Column(db.Integer)
+    numAno = db.Column(db.Integer)
+    deputado_id = db.Column(db.Integer)
+
+
+    def __init__(self, numRessarcimento, deputado_id, sgUf, sgPartido, txtDescricao, txtFornecedor, vlrLiquido, numMes,
+                 numAno):
+        self.numRessarcimento = numRessarcimento
+        self.deputado_id = deputado_id
+        self.sgUF = sgUf
+        self.sgPartido = sgPartido
+        self.txtDescricao = txtDescricao
+        self.txtFornecedor = txtFornecedor
+        self.vlrLiquido = vlrLiquido
+        self.numMes = numMes
+        self.numAno = numAno
+
+    @classmethod
+    def atualiza_database(self):
+        db.create_all()
+        tree = etree.parse("D:\eParlamentar\AnoAtual.xml")
+        despesas = tree.getroot()[0]
+
+        for despesa in despesas:
+            numRessarcimento = int(despesa.find('numRessarcimento').text)
+            sgUF = despesa.find('sgUF').text
+            sgPartido = despesa.find('sgPartido').text
+            txtDescricao = despesa.find('txtDescricao').text
+            txtFornecedor = despesa.find('txtFornecedor').text
+            vlrLiquido = float(despesa.find('vlrLiquido').text)
+            numMes = despesa.find('numMes').text
+            numAno = despesa.find('numAno').text
+            deputado_id = int(despesa.find('ideCadastro').text)
+            d = Despesa(numRessarcimento,deputado_id,sgUF,sgPartido,txtDescricao,txtFornecedor,vlrLiquido,numMes,numAno)
+            db.session.add(d)
+            db.session.commit()
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Despesa %r>' % self.numRessarcimento
